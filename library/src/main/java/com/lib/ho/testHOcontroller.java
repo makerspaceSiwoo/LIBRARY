0a22d6a2a26d5fe96561e5bb056852b2aec452b9
ho <shy80695@gmail.com>
2024-08-05 17:58:01 +0900

    ho controller update

--- library/src/main/java/com/lib/ho/testHOcontroller.java
+++ library/src/main/java/com/lib/ho/testHOcontroller.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class testHOcontroller {
 	@GetMapping("/ho")
 	public String ho() {
+		System.out.println("ho");
 		return "home";
 	}
+	
 }
