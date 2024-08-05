bc79e3c8dc58598294556f3ee626b30fb972f4d6
HAJIHYUK <gkwlgurdydydy@naver.com>
2024-08-05 17:16:25 +0900

    add 컨트롤러 by jh

---
+++ library/src/main/java/com/lib/jh/controller/JController.java
@@ -0,0 +1,12 @@
+package com.lib.jh.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JController {
+
+	public String test() {
+		return "test";
+	}
+	
+}
