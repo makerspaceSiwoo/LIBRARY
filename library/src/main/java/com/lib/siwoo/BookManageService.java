4bef4a5e5c895ec83d676e7ac6f278903ebe0e49
82102 <82102@DESKTOP-BH30DS5>
2024-08-07 17:33:25 +0900

    book manage controller

--- library/src/main/java/com/lib/siwoo/BookManageService.java
+++ library/src/main/java/com/lib/siwoo/BookManageService.java
@@ -1,8 +1,27 @@
 package com.lib.siwoo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.lib.dto.BookDto;
 
 @Service
 public class BookManageService {
+	
+	@Autowired
+	BookManageDao dao;
+	
+	// 이미 있는 책인지 확인
+	public int insertBook(BookDto dto) {
+		// dto의 칼럼이 null인 경우, 접근이 실패할 수 있음
+		try {
+			if(dao.checkDuplicate(dto.getCallno()) == 0) { // 청구 기호로 검색해서 이미 존재하는지 확인
+				return dao.insertBook(dto);
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return -1; // 이미 존재하는 책이면 db에 추가하지 않고, -1 리턴
+	}
 
 }
