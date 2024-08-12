3decef8f710ce5167c236bed8fa6641b4d88019c
ksm <fpdlqms0226@naver.com>
2024-08-12 10:00:21 +0900

    search book

--- library/src/main/java/com/lib/mo/dao/SearchDao.java
+++ library/src/main/java/com/lib/mo/dao/SearchDao.java
@@ -10,17 +10,16 @@ import com.lib.dto.BookDto;
 
 @Mapper
 public interface SearchDao {
-	
+
 	@Select({ "<script>select * from book",
-		"<where><choose><when test=\"searchn == 0\">booktitle like concat('%',#{search},'%')</when>",
-		"<when test=\"searchn == 1\">category like concat('%',#{search},'%')</when>",
-		"<when test=\"searchn == 2\">author like concat('%',#{search},'%')</when>",
-		"</choose></where> order by booktitle limit #{start}, #{count}</script>"})
+			"<where><choose><when test=\"searchn == 0\">booktitle like concat('%',#{search},'%') and category like concat('%',#{cate},'%')</when>",
+			"<when test=\"searchn == 1\">author like concat('%',#{search},'%') and category like concat('%',#{cate},'%')</when>",
+			"</choose></where> order by booktitle limit #{start}, #{count}</script>" })
 	List<BookDto> searchBook(Map<String, Object> params);
-	
-	@Select({ "<script>select count(*) from book", "<where>","<choose>",
-		"<when test=\"searchn == 0\">booktitle like concat('%',#{search},'%')</when>",
-		"<when test=\"searchn == 1\">category like concat('%',#{search},'%') </when>",
-		"<when test=\"searchn == 2\">author like concat('%',#{search},'%') </when>", "</choose></where></script>" })
-	int countBook(Map<String, Object>m);
+
+	@Select({ "<script>select count(*) from book", "<where>", "<choose>",
+			"<when test=\"searchn == 0\">booktitle like concat('%',#{search},'%') and category like concat('%',#{cate},'%')</when>",
+			"<when test=\"searchn == 1\">author like concat('%',#{search},'%') and category like concat('%',#{cate},'%')</when>",
+			"</choose></where></script>" })
+	int countBook(Map<String, Object> m);
 }
